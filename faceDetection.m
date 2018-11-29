ef04c289d58bed25f9846e76a7e5587760a1088c
unknown <kacperwoloszyn4@gmail.com>
2018-11-29 12:12:41 +0000

    pt tracking

--- faceDetection.m
+++ faceDetection.m
@@ -1,5 +1,10 @@
 % Create the face detector object.
 faceDetector = vision.CascadeObjectDetector();
+
+% Create the point tracker object.
+pointTracker = vision.PointTracker('MaxBidirectionalError', 2);
+numPts=0;
+
 % Clear any active camera connections 
 clear cam;
 
@@ -21,12 +26,74 @@ while run
 
      % Detect bounding boxs for face from grey image.
      bbox = faceDetector.step(videoFrameGray);
-
+     
      % if we detect faces, insert a rectange into frame
+    if numPts < 10   
+     % Detect bounding boxs for face from grey image.
+     bbox = faceDetector.step(videoFrameGray);
+
      if ~isempty(bbox)
-     % insert bounding box around the detected face.
-            videoFrame = insertShape(videoFrame, 'Rectangle', bbox, 'LineWidth', 3);
+                 % Find corner points inside the detected region.
+            points = detectMinEigenFeatures(videoFrameGray, 'ROI', bbox(1, :));
+
+            % Re-initialize the point tracker.
+            xyPoints = points.Location;
+            numPts = size(xyPoints,1);
+            release(pointTracker);
+            initialize(pointTracker, xyPoints, videoFrameGray);
+
+            % Save a copy of the points.
+            oldPoints = xyPoints;
+
+            % Convert the rectangle represented as [x, y, w, h] into an
+            % M-by-2 matrix of [x,y] coordinates of the four corners. This
+            % is needed to be able to transform the bounding box to display
+            % the orientation of the face.
+            bboxPoints = bbox2points(bbox(1, :));  
+
+            % Convert the box corners into the [x1 y1 x2 y2 x3 y3 x4 y4] 
+            % format required by insertShape.
+            bboxPolygon = reshape(bboxPoints', 1, []);
+
+            % Display a bounding box around the detected face.
+            videoFrame = insertShape(videoFrame, 'Polygon', bboxPolygon, 'LineWidth', 3);
+
+            % Display detected corners.
+            videoFrame = insertMarker(videoFrame, xyPoints, '+', 'Color', 'white');
      end
+
+  else
+      % Tracking mode.
+        [xyPoints, isFound] = step(pointTracker, videoFrameGray);
+        visiblePoints = xyPoints(isFound, :);
+        oldInliers = oldPoints(isFound, :);
+
+        numPts = size(visiblePoints, 1);       
+
+        if numPts >= 10
+            % Estimate the geometric transformation between the old points
+            % and the new points.
+            [xform, oldInliers, visiblePoints] = estimateGeometricTransform(...
+                oldInliers, visiblePoints, 'similarity', 'MaxDistance', 4);            
+
+            % Apply the transformation to the bounding box.
+            bboxPoints = transformPointsForward(xform, bboxPoints);
+
+            % Convert the box corners into the [x1 y1 x2 y2 x3 y3 x4 y4] 
+            % format required by insertShape.
+            bboxPolygon = reshape(bboxPoints', 1, []);            
+
+            % Display a bounding box around the face being tracked.
+            videoFrame = insertShape(videoFrame, 'Polygon', bboxPolygon, 'LineWidth', 3);
+
+            % Display tracked points.
+            videoFrame = insertMarker(videoFrame, visiblePoints, '+', 'Color', 'white');
+
+            % Reset the points.
+            oldPoints = visiblePoints;
+            setPoints(pointTracker, oldPoints);
+        end
+  end
     % Display the annotated video frame using the video player object.
     step(videoPlayer, videoFrame);
 
